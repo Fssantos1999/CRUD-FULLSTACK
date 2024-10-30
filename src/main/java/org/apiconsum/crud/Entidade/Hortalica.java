0a7f46863729e3784ee7a57838b3c77d5e56a21f
Felipe <felipecunhassantos@gmail.com>
2024-10-30 16:51:46 -0300

    Adiciona tela de boas-vindas com animação de partículas e atualizações de estilo CSS

---
+++ src/main/java/org/apiconsum/crud/Entidade/Hortalica.java
@@ -0,0 +1,91 @@
+package org.apiconsum.crud.Entidade;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String medidasProtetivas;
+
+    @Column(nullable = false)
+    private String tipoSoloPH;
+
+    @Column(nullable = false)
+    private String melhorEpoca;
+
+    @Column(nullable = false)
+    private double preco;
+
+
+    public Produto(Long id, String nome, String medidasProtetivas, String tipoSoloPH, String melhorEpoca, double preco) {
+        this.id = id;
+        this.nome = nome;
+        this.medidasProtetivas = medidasProtetivas;
+        this.tipoSoloPH = tipoSoloPH;
+        this.melhorEpoca = melhorEpoca;
+        this.preco = preco;
+    }
+
+
+    public Produto() {
+
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMedidasProtetivas() {
+        return medidasProtetivas;
+    }
+
+    public void setMedidasProtetivas(String medidasProtetivas) {
+        this.medidasProtetivas = medidasProtetivas;
+    }
+
+    public String getTipoSoloPH() {
+        return tipoSoloPH;
+    }
+
+    public void setTipoSoloPH(String tipoSoloPH) {
+        this.tipoSoloPH = tipoSoloPH;
+    }
+
+    public String getMelhorEpoca() {
+        return melhorEpoca;
+    }
+
+    public void setMelhorEpoca(String melhorEpoca) {
+        this.melhorEpoca = melhorEpoca;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+}
